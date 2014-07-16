01e2afe090adb5f4bdde02fbb58e03e809d85ef7
David Christenson <david@the-david.com>
2014-07-16 16:34:33 -0600

    rar 5.1.0
    Update to latest release, use Homebrew's compiled unrar by default,
    add conflict for new binary unrar option.

--- rar.rb
+++ rar.rb
@@ -1,20 +1,33 @@
-require 'formula'
+require "formula"
 
 class Rar < Formula
-  url 'http://www.rarlab.com/rar/rarosx-5.0.1.tar.gz'
-  homepage 'http://www.rarlab.com'
-  sha1 '0f93f8abdacbf9e77273a056ada1bcd182118ba3'
+  homepage "http://www.rarlab.com"
+  url "http://www.rarlab.com/rar/rarosx-5.1.0.tar.gz"
+  sha1 "819ce9205816c8a3c1365ea75ea49ed515271b18"
 
-  resource 'man' do
-    url 'http://manpages.ubuntu.com/manpages.gz/trusty/man1/rar.1.gz'
-    sha1 'f3feaef47d7dfa5a3a8372153c743921ed3eda51'
+  option "with-binary-unrar", "Install binary unrar instead of using Homebrew's compilation"
+
+  if build.with? "binary-unrar"
+    conflicts_with "unrar", :because => "Both install an unrar binary"
+  else
+    depends_on "unrar"
+  end
+
+  resource "man" do
+    url "http://manpages.ubuntu.com/manpages.gz/trusty/man1/rar.1.gz"
+    sha1 "f3feaef47d7dfa5a3a8372153c743921ed3eda51"
   end
 
   def install
-    bin.install 'rar', 'unrar'
-    lib.install 'default.sfx'
-    etc.install 'rarfiles.lst'
-    doc.install 'acknow.txt', 'license.txt', 'order.htm', 'rar.txt', 'readme.txt', 'whatsnew.txt'
-    man1.install resource('man')
+    if build.with? "binary-unrar"
+      bin.install "rar", "unrar"
+    else
+      bin.install "rar"
+    end
+
+    lib.install "default.sfx"
+    etc.install "rarfiles.lst"
+    doc.install "acknow.txt", "license.txt", "order.htm", "rar.txt", "readme.txt", "whatsnew.txt"
+    man1.install resource("man")
   end
 end
